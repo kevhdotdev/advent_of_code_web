e879e227eba918d7bd66c4734117c0ad73d9889e
Kevin Hughes <kevtufc@gmail.com>
2020-12-01 13:21:39 +0000

    Day One. Part One.

--- lib/aoc/Y2020/day01.ex
+++ lib/aoc/Y2020/day01.ex
@@ -1,8 +1,21 @@
 defmodule Aoc.Y2020.Day01 do
   @behaviour Aoc.Day
 
+  @doc ~S"""
+  ## Examples
+
+      iex> part_one("1721\n979\n366\n299\n675\n1456\n514579\n")
+      514579
+  """
   @impl true
-  def part_one(_input) do
+  def part_one(input) do
+    expenses = input |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1)
+
+    try do
+      for x <- expenses, y <- expenses, do: if(x + y == 2020, do: throw(x * y))
+    catch
+      x -> x
+    end
   end
 
   @impl true
--- test/aoc/doc_test.exs
+++ test/aoc/doc_test.exs
@@ -2,6 +2,6 @@ defmodule Aoc.DocTest do
   use ExUnit.Case, async: true
 
   for year <- [2020], day <- 1..25 do
-    doctest :"Elixir.Aoc.Y#{year}.Day#{Aoc.Input.padded_number(day)}"
+    doctest :"Elixir.Aoc.Y#{year}.Day#{Aoc.Input.padded_number(day)}", import: true
   end
 end
