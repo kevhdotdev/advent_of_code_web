1dc3e64297dc5118d1f1bfec9ea79c611ad3c320
Kevin Hughes <kevtufc@gmail.com>
2020-12-01 13:24:15 +0000

    Day One. Part Two.

--- lib/aoc/Y2020/day01.ex
+++ lib/aoc/Y2020/day01.ex
@@ -18,7 +18,23 @@ defmodule Aoc.Y2020.Day01 do
     end
   end
 
+  @doc ~S"""
+  ## Examples
+
+  iex> part_two("1721\n979\n366\n299\n675\n1456\n514579\n")
+  241861950
+  """
   @impl true
-  def part_two(_input) do
+  def part_two(input) do
+    expenses = input |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1)
+
+    try do
+      for x <- expenses,
+          y <- expenses,
+          z <- expenses,
+          do: if(x + y + z == 2020, do: throw(x * y * z))
+    catch
+      x -> x
+    end
   end
 end
