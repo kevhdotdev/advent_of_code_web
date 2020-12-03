1966acf1d9ee32b754b62ff6490117a76683f1af
Kevin Hughes <kevtufc@gmail.com>
2020-12-03 11:32:55 +0000

    Redo Day One. Happy now, Dr Nik?
    Fixes #1

--- elixir/lib/aoc/Y2020/day01.ex
+++ elixir/lib/aoc/Y2020/day01.ex
@@ -4,37 +4,46 @@ defmodule Aoc.Y2020.Day01 do
   @doc ~S"""
   ## Examples
 
-      iex> part_one("1721\n979\n366\n299\n675\n1456\n514579\n")
+      iex> part_one("1721\n979\n366\n299\n675\n1456\n")
+      514579
+
+      # Duplicates don't count
+      iex> part_one("1010\n1721\n979\n366\n299\n675\n1456\n")
       514579
   """
   @impl true
   def part_one(input) do
-    expenses = input |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1)
+    input |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1) |> match
+  end
 
-    try do
-      for x <- expenses, y <- expenses, do: if(x + y == 2020, do: throw(x * y))
-    catch
-      x -> x
-    end
+  defp match([h | t]) do
+    if((2020 - h) in t, do: h * (2020 - h), else: match(t))
   end
 
   @doc ~S"""
   ## Examples
 
-  iex> part_two("1721\n979\n366\n299\n675\n1456\n514579\n")
-  241861950
+      iex> part_two("1721\n979\n366\n299\n675\n1456\n")
+      241861950
+
+      # Duplicates don't count
+      iex> part_two("564\n1721\n979\n366\n299\n675\n1456\n")
+      241861950
   """
   @impl true
   def part_two(input) do
-    expenses = input |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1)
+    input |> String.trim() |> String.split() |> Enum.map(&String.to_integer/1) |> match3()
+  end
 
-    try do
-      for x <- expenses,
-          y <- expenses,
-          z <- expenses,
-          do: if(x + y + z == 2020, do: throw(x * y * z))
-    catch
-      x -> x
+  defp match3(list), do: match3(list, list)
+
+  defp match3([_, h | t], []), do: match3([h | t], t)
+
+  defp match3([h1 | t1], [h2 | t2]) do
+    if((2020 - h1 - h2) in t2) do
+      h1 * h2 * (2020 - h1 - h2)
+    else
+      match3([h1 | t1], t2)
     end
   end
 end
