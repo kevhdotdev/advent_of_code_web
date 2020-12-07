0ad5fff3f3dcc7890ce5f6b7dae221842148592d
Kevin Hughes <kevtufc@gmail.com>
2020-12-07 11:02:09 +0000

    Day Seven Part Two

--- elixir/lib/aoc/Y2020/day07.ex
+++ elixir/lib/aoc/Y2020/day07.ex
@@ -11,25 +11,11 @@ defmodule Aoc.Y2020.Day07 do
     tree =
       input
       |> String.split("\n", trim: true)
-      |> parse
+      |> parse(1)
 
     tree
     |> can_contain(tree, "shiny gold", MapSet.new())
     |> MapSet.size()
-  end
-
-  defp parse([]), do: []
-
-  defp parse([h | t]) do
-    [parent, rest] = String.split(h, " bags contain ")
-
-    [
-      {parent,
-       for child <- String.split(rest, ~r/ bags?[,.] ?/), child != "" && child != "no other" do
-         child |> String.split(" ", parts: 2) |> List.last()
-       end}
-      | parse(t)
-    ]
   end
 
   defp can_contain([], _, _, answers), do: answers
@@ -45,7 +31,41 @@ defmodule Aoc.Y2020.Day07 do
     |> MapSet.union(t |> can_contain(tree, bag, answers))
   end
 
+  @doc ~S"""
+  ## Examples
+  iex> part_two "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags.\n"
+  32
+  """
   @impl true
-  def part_two(_input) do
+  def part_two(input) do
+    tree =
+      input
+      |> String.split("\n", trim: true)
+      |> parse(2)
+      |> contains("shiny gold")
+  end
+
+  defp parse([], _), do: []
+
+  defp parse([h | t], part) do
+    [parent, rest] = String.split(h, " bags contain ")
+
+    [
+      {parent,
+       for child <- String.split(rest, ~r/ bags?[,.] ?/), child != "" && child != "no other" do
+         [count, colour] = String.split(child, " ", parts: 2)
+         if part == 1, do: colour, else: {colour, String.to_integer(count)}
+       end}
+      | parse(t, part)
+    ]
+  end
+
+  def contains(tree, parent) do
+    {^parent, children} = Enum.find(tree, fn {p, _} -> p == parent end)
+
+    for {child, count} <- children do
+      count + count * contains(tree, child)
+    end
+    |> Enum.sum()
   end
 end
